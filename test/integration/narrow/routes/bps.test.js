5c64bc4ec6502994037c98767ce06c44b8c9dd8e
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-02-23 11:45:14 +0000

    fix: linting issues

--- test/integration/narrow/routes/bps.test.js
+++ test/integration/narrow/routes/bps.test.js
@@ -1,84 +1,81 @@
-const { testEnvironment } = require('../../../../jest.config')
-
 describe('bps route', () => {
-    let createServer
-    let server
+  let createServer
+  let server
 
-    beforeEach(async () => {
-        createServer =  require('../../../../app/server')
-        server = await createServer()
-        await server.initialize()
-    })
+  beforeEach(async () => {
+    createServer = require('../../../../app/server')
+    server = await createServer()
+    await server.initialize()
+  })
 
-    afterEach(async () => {
-        await server.stop()
-    })
+  afterEach(async () => {
+    await server.stop()
+  })
 
-    test('GET /bps returns 200', async () => {
-        const options = {
-            method: 'GET',
-            url: '/bps'
-        }
+  test('GET /bps returns 200', async () => {
+    const options = {
+      method: 'GET',
+      url: '/bps'
+    }
 
-        const result = await server.inject(options)
-        expect(result.statusCode).toBe(200)
-    })
+    const result = await server.inject(options)
+    expect(result.statusCode).toBe(200)
+  })
 
-    test('GET / returns home view', async () => {
-        const options = {
-            method: 'GET',
-            url: '/bps'
-        }
+  test('GET / returns home view', async () => {
+    const options = {
+      method: 'GET',
+      url: '/bps'
+    }
 
-        const result = await server.inject(options)
-        expect(result.request.response.variety).toBe('view')
-        expect(result.request.response.source.template).toBe('bps')
-    })
+    const result = await server.inject(options)
+    expect(result.request.response.variety).toBe('view')
+    expect(result.request.response.source.template).toBe('bps')
+  })
 
-    test('GET / context includes Header', async () => {
-        const options = {
-            method: 'GET',
-            url: '/bps'
-        }
+  test('GET / context includes Header', async () => {
+    const options = {
+      method: 'GET',
+      url: '/bps'
+    }
 
-        const result = await server.inject(options)
-        expect(result.request.response._payload._data).toContain('Calculate my payments')
-    })
+    const result = await server.inject(options)
+    expect(result.request.response._payload._data).toContain('Calculate my payments')
+  })
 
-    test('POST /bps returns 302', async () => {
-        const options = {
-            method: 'POST',
-            url: '/bps',
-            payload: { bpsValue: 1000.01 }
-        }
+  test('POST /bps returns 302', async () => {
+    const options = {
+      method: 'POST',
+      url: '/bps',
+      payload: { bpsValue: 1000.01 }
+    }
 
-        const result = await server.inject(options)
-        expect(result.statusCode).toBe(302)
-    })
+    const result = await server.inject(options)
+    expect(result.statusCode).toBe(302)
+  })
 
-    // test('POST /bps invalid', async () => {
-    //     const options = {
-    //         method: 'POST',
-    //         url: '/bps',
-    //         payload: { bpsValue: 'aaaaaa' }
-    //     }
+  // test('POST /bps invalid', async () => {
+  //     const options = {
+  //         method: 'POST',
+  //         url: '/bps',
+  //         payload: { bpsValue: 'aaaaaa' }
+  //     }
 
-    //     const result = await server.inject(options)
-    //     expect(result.statusCode).toBe(302)
-    // })
+  //     const result = await server.inject(options)
+  //     expect(result.statusCode).toBe(302)
+  // })
 
-    test('POST /bps redirects to calculation', async () => {
+  test('POST /bps redirects to calculation', async () => {
+    const bpsValue = 1000.01
 
-        const bpsValue = 1000.01
+    const options = {
+      method: 'POST',
+      url: '/bps',
+      payload: { bpsValue }
+    }
 
-        const options = {
-            method: 'POST',
-            url: '/bps',
-            payload: { bpsValue }
-        }
-
-        const result = await server.inject(options)
-        expect(result.statusCode).toBe(302)
-        expect(result.headers.location).toBe('/calculation?bpsValue=' + bpsValue)
-    })
+    const result = await server.inject(options)
+    expect(result.statusCode).toBe(302)
+    expect(result.headers.location).toBe('/calculation?bpsValue=' + bpsValue)
+  })
 })
--- test/integration/narrow/routes/calculation.test.js
+++ test/integration/narrow/routes/calculation.test.js
@@ -1,49 +1,47 @@
-const { testEnvironment } = require('../../../../jest.config')
-
 describe('calculation route', () => {
-    let createServer
-    let server
+  let createServer
+  let server
 
-    beforeEach(async () => {
-        createServer =  require('../../../../app/server')
-        server = await createServer()
-        await server.initialize()
-    })
+  beforeEach(async () => {
+    createServer = require('../../../../app/server')
+    server = await createServer()
+    await server.initialize()
+  })
 
-    afterEach(async () => {
-        await server.stop()
-    })
+  afterEach(async () => {
+    await server.stop()
+  })
 
-    test('GET / returns 200', async () => {
-        const options = {
-            method: 'GET',
-            url: '/calculation'
-        }
+  test('GET / returns 200', async () => {
+    const options = {
+      method: 'GET',
+      url: '/calculation'
+    }
 
-        const result = await server.inject(options)
-        expect(result.statusCode).toBe(200)
-    })
+    const result = await server.inject(options)
+    expect(result.statusCode).toBe(200)
+  })
 
-    test('GET / returns calculation view', async () => {
-        const options = {
-            method: 'GET',
-            url: '/calculation'
-        }
+  test('GET / returns calculation view', async () => {
+    const options = {
+      method: 'GET',
+      url: '/calculation'
+    }
 
-        const result = await server.inject(options)
-        expect(result.request.response.variety).toBe('view')
-        expect(result.request.response.source.template).toBe('calculation')
-    })
+    const result = await server.inject(options)
+    expect(result.request.response.variety).toBe('view')
+    expect(result.request.response.source.template).toBe('calculation')
+  })
 
-    test('GET / includes table', async () => {
-        const options = {
-            method: 'GET',
-            url: '/calculation'
-        }
+  test('GET / includes table', async () => {
+    const options = {
+      method: 'GET',
+      url: '/calculation'
+    }
 
-        const result = await server.inject(options)
-        expect(result.request.response.variety).toBe('view')
-        expect(result.request.response.source.template).toBe('calculation')
-        expect(result.payload).toContain('govuk-table')
-    })
+    const result = await server.inject(options)
+    expect(result.request.response.variety).toBe('view')
+    expect(result.request.response.source.template).toBe('calculation')
+    expect(result.payload).toContain('govuk-table')
+  })
 })
