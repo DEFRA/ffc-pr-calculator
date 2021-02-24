20876adb8c63e518e182f2c8f3f5314f4257d79b
steve-dickinson <dichiohare@googlemail.com>
2021-02-24 21:37:16 +0000

    Refectored and styled

--- app/routes/models/calculation.js
+++ app/routes/models/calculation.js
@@ -1,142 +1,77 @@
 
 const bands = require('../../calculation/bands')
 
-function GetBandText(band) {
+function GetBandText (band) {
   return bands.find(x => x.band === band).text
 }
 
-function reductionToRow (bandResults) {
+function toRow (results, property, formatType) {
   const data = []
-  data.push({ text: GetBandText(bandResults.band) })
-  bandResults.result.map(x => data.push({ text: `£${x.reduction.toFixed(2)}` } ))
+  data.push({ text: GetBandText(results.band) })
+  results.result.map(x => data.push(
+    { 
+      text: (formatType === 'currency'  ? 
+      `£${x[property].toFixed(2)}` : 
+      `${Math.round(x[property] * 100)}%`), format: "numeric" } ))
   return data
 }
 
-function reductionOverallToRow (overallResult) {
+function overallToRow (overallResult, property) {
   const data = []
-  data.push({text : `£${overallResult.reduction.toFixed(2)}`})
+  data.push({text : `£${overallResult[property].toFixed(2)}`, 
+            format: "numeric", 
+            classes: "govuk-body govuk-!-font-weight-bold"})
   return data
 }
 
-function paymentOverallToRow (overallResult) {
-  const data = []
-  data.push({text : `£${overallResult.payment.toFixed(2)}`})
-  return data
+function populateOverall (calculations, property, text) {
+  const overall = calculations.overallResult.map(x => overallToRow(x, property))
+  overall.unshift({text: text})
+  return overall
 }
 
-function populateReductionData (calculations) {
-  const reductionData = calculations.bandResult.map(reductionToRow)
-  const overall = calculations.overallResult.map(reductionOverallToRow)
-  overall.unshift({text: 'Total progressive reduction:'})
-  reductionData.push(overall.flat())
+function populateData (calculations, property, text, formatType, showOverall) {
+  const reductionData = calculations.bandResult.map(x => toRow(x, property, formatType))
+  if(showOverall) {  
+    reductionData.push(populateOverall(calculations, property, text).flat())
+  }
   return reductionData
 }
 
-function paymentToRow (calculation) {
-  const data = []
-  data.push({ text: GetBandText(calculation.band) })
-  calculation.result.map(x => data.push({ text: `£${x.payment.toFixed(2)}` }))
-  return data
-}
-
-function populatePaymentBandData (calculations) {
-  return calculations.bandResult.map(paymentBandToRow)
-}
-
-function populatePaymentData (calculations) {
-  const paymentData = calculations.bandResult.map(paymentToRow)
-  const overall = calculations.overallResult.map(paymentOverallToRow)
-  overall.unshift({text: 'Payment value after progressive reductions:'})
-  paymentData.push(overall.flat())
-  return paymentData
-}
-
-function paymentBandToRow (calculation) {
-  const data = []
-  data.push({ text: GetBandText(calculation.band) })
-  calculation.result.map(x => data.push({ text: `${x.rate * 100}%` }))
-  console.log(calculation.result)
-  return data
-}
-
-function createReductionTableDefinition (calculations) {
+function createTableDefinition (calculations, property, text, caption, formatType = 'currency', showOverall = true) {
   return {
-    caption: 'Reductions within each band',
+    caption: caption,
     captionClasses: 'govuk-table__caption--l',
     firstCellIsHeader: true,
     head: [
       {
-        text: 'scheme year'
+        text: 'scheme year',
+        classes: 'govuk-!-width-one-half'
       },
       {
-        text: '2021'
+        text: '2021',
+        format: "numeric"
       },
       {
-        text: '2022'
+        text: '2022',
+        format: "numeric"
       },
       {
-        text: '2023'
+        text: '2023',
+        format: "numeric"
       },
       {
-        text: '2024'
+        text: '2024',
+        format: "numeric"
       }
     ],
-    rows: populateReductionData(calculations)
-  }
-}
-
-function createPaymentBandTableDefinition (calculations) {
-  return {
-    caption: 'Payment band',
-    captionClasses: 'govuk-table__caption--l',
-    firstCellIsHeader: true,
-    head: [
-      {
-        text: 'scheme year'
-      },
-      {
-        text: '2021'
-      },
-      {
-        text: '2022'
-      },
-      {
-        text: '2023'
-      },
-      {
-        text: '2024'
-      }
-    ],
-    rows: populatePaymentBandData(calculations)
-  }
-}
-
-function createPaymentTableDefinition (calculations) {
-  return {
-    caption: 'Payments within each band',
-    captionClasses: 'govuk-table__caption--l',
-    firstCellIsHeader: true,
-    head: [
-      {
-        text: 'scheme year'
-      },
-      {
-        text: '2021'
-      },
-      {
-        text: '2022'
-      },
-      {
-        text: '2023'
-      },
-      {
-        text: '2024'
-      }
-    ],
-    rows: populatePaymentData(calculations)
+    rows: populateData(calculations, property, text, formatType, showOverall)
   }
 }
 
 module.exports = function ViewModel (calculations) {
-  this.model = { paymentBand: createPaymentBandTableDefinition(calculations), reduction: createReductionTableDefinition(calculations), payment: createPaymentTableDefinition(calculations) }
+  this.model = { 
+    paymentBand: createTableDefinition(calculations, 'rate', '', 'Payment band', 'percentage', false), 
+    payment: createTableDefinition(calculations, 'payment', 'Payment value after progressive reductions:', 'Payments within each band'), 
+    reduction: createTableDefinition(calculations, 'reduction', 'Total progressive reduction:', 'Reductions within each band') }
 }
