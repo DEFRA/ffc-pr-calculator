bcb5e387f8f42e121b7679d0948442e5be93e3f0
kaziyiola <kaz.iyiola@defra.gov.uk>
2021-02-25 07:48:12 +0000

    Inital tests

---
+++ test/acceptance/pageobjects/bspvalue.page.js
@@ -0,0 +1,15 @@
+class BspValuePage extends Page {
+   
+    get homeLink () { return $('a.govuk-breadcrumbs__link') }
+    get serviceLink () { return $('a.govuk-link') }
+    get licenseLink () {return $('//span/a')}
+    get titleHeader () {return $('h1.govuk-panel__title')}
+
+    verifyTitlePageText() {
+        this.titleHeader.getText();
+    }     
+}
+
+export default new BspValuePage();
+                   
+
---
+++ test/acceptance/stepdefinitions/prcalculatorsteps.js
@@ -0,0 +1,33 @@
+import { Given, When, Then} from 'cucumber';
+
+
+import HomePage from '../pageobjects/home.page';
+import BspPage from '../pageobjects/bsp.page';
+import BspValuePage from '../pageobjects/bspvalue.page';
+
+const pages = {
+    home: HomePage
+}
+        
+Given(/^that I am on the Progressive Reductions Calculator (\w+) page$/, async (page) => {
+    await pages[page].open()
+});
+
+When(/^I click on start new calculator button$/, async () => {
+    HomePage.startNewCalBtn();
+});
+
+When(/^I enter BPS payment value (.*)$/, async (amount) => {
+    BspPage.enterBpsValue(amount);
+});
+
+When(/^I click calculate my payment $/, async () => {
+    BspPage.calculateBpsValue();
+});
+
+Then(/^I should see (.*)$/, async (message) => {
+     expect(BspValuePage.verifyTitlePageText).toHaveTextContaining(message);
+});
+
+
+
