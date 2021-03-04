7ed620645588741304b680b59e29d987105f97e7
kaziyiola <kaz.iyiola@defra.gov.uk>
2021-03-04 10:18:28 +0000

    Scenarios test

--- test/acceptance/pageobject/Bsp.page.js
+++ test/acceptance/pageobject/Bsp.page.js
@@ -1,24 +1,48 @@
-class Bsppage  {
+import Page from './page';
+import { expect } from 'chai';
 
-    get inputBPSValue () { return $('#bpsValue') }
-    get calMyPaymentBtn () { return $('button.govuk-button') }
-    get homeLink () {return $('button.govuk-button')}
-    
-    
+class bspPage extends Page {
 
-    async enterBpsValue() {
-        await (await this.inputBPSValue).setValue('500');
-                     
+    get inputBPSValue () { return $('#bpsValue'); }
+    get calMyPaymentBtn () { return $('button.govuk-button'); }
+    get homeLink () {return $('button.govuk-button');}
+    get errorTextMessage () {return $('span#bpsValue-error.govuk-error-message');}
+
+
+    async enterBpsValue(amount) {
+        await (await this.inputBPSValue).setValue(amount);                    
     }
 
     async calculateBpsValue() {
         this.calMyPaymentBtn.click();
     }
 
-    // async start () {
-    //     await (await this.startNewCalBtn).click();
-    // }
+    async getErrorTextMessage(errormessage) {
+        //(await this.errorTextMessage).getText();
+        expect(errorTextMessage).to.equal(errormessage);
+        expect(errorTextMessage).toHaveText(errormessage);
+    }
 
+    async clickOnHomeLink() {
+        await (await this.homeLink.click());
+    }
+
+    open () {
+        super.open('calculation?bpsValue=500')
+      }
+
+      open () {
+        super.open('http://host.docker.internal:3000')
+        //browser.pause(3000)
+      }
 }
 
-export default new Bsppage();
+export default new bspPage();
+
+
+
+ 
+  
+
+
+
