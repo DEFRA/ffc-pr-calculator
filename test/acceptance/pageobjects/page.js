3b2faaed24ad8161a471acc079b1bc03e89ce9a9
kaziyiola <kaz.iyiola@defra.gov.uk>
2021-03-04 13:50:51 +0000

    scenario add

--- test/acceptance/pageobjects/page.js
+++
@@ -1,9 +0,0 @@
-export default class Page {
-   
-    open (path) {
-       //return browser.url(`http://host.docker.internal:3000/${path}`)     https://www.google.co.uk/
-       // browser.url(path)
-
-       return browser.url(`http://localhost:3000/${path}`);
-    }
-}
