7c68227767f2899171e8a05cb8c7a73c83013ac9
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-03-09 11:50:41 +0000

    Added error validation and thousands seperator

--- app/routes/models/bps-multiple.js
+++ app/routes/models/bps-multiple.js
@@ -15,19 +15,23 @@ function createInput (paymentBand, error, value) {
   }
 }
 
-module.exports = function ViewModel (payload, error) {
-  const errorMessage = { text: '' }
+function createErrorSummary (errorDetails) {
+  if (errorDetails) {
+    const errorList = errorDetails.map((error) => ({ text: error.message, href: `#${error.path}` }))
 
-  // If error is passed to model then this error property is added to the model
-  if (error) {
-    errorMessage.text = 'Your BPS value must be greater than £0 and less than £1,000,000,000'
+    return {
+      titleText: 'There is a problem',
+      errorList
+    }
   }
+}
 
+module.exports = function ViewModel (payload, error) {
   this.model = {
     band2021: createInput('2021', error, payload?.bps2021Value),
     band2022: createInput('2022', error, payload?.bps2022Value),
     band2023: createInput('2023', error, payload?.bps2023Value),
     band2024: createInput('2024', error, payload?.bps2024Value),
-    errorMessage
+    errors: createErrorSummary(error?.details)
   }
 }
