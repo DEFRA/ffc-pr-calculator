c60122ff610c9d394f894c981e3ea1a5201e9716
John Watson <johnwatson484@hotmail.com>
2021-03-09 14:44:24 +0000

    Remove invalid tests

--- test/integration/narrow/app/server.test.js
+++ test/integration/narrow/app/server.test.js
@@ -3,27 +3,9 @@ describe('Server test', () => {
 
   beforeEach(() => {
     jest.resetModules()
-    jest.mock('../../../../app/plugins/router', () => {
-      return {
-        plugin: {
-          name: 'mock-router',
-          register: () => {}
-        }
-      }
-    })
   })
 
   test('createServer returns server', () => {
-    server = require('../../../../app/server')
-    expect(server).toBeDefined()
-  })
-
-  test('createServer returns server if dev', async () => {
-    jest.mock('../../../../app/config', () => {
-      return {
-        isDev: true
-      }
-    })
     server = require('../../../../app/server')
     expect(server).toBeDefined()
   })
