b4598517709ae2475c2856dcfa95fcdd08da067f
John Watson <johnwatson484@hotmail.com>
2021-03-09 14:12:11 +0000

    Server test added

--- test/integration/narrow/app/server.test.js
+++ test/integration/narrow/app/server.test.js
@@ -3,4 +3,16 @@ describe('Server test', () => {
     const server = require('../../../../app/server')
     expect(server).toBeDefined()
   })
+
+  test('entry point starts server', () => {
+    const mockStart = jest.fn()
+    jest.mock('../../../../app/server', () => jest.fn(() => {
+      return {
+        then: mockStart.mockReturnThis(),
+        catch: jest.fn()
+      }
+    }))
+    require('../../../../app')
+    expect(mockStart.mock.calls.length).toBe(1)
+  })
 })
