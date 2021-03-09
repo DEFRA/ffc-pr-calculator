e972f756d48854adbac099ea71322eacb480a95a
John Watson <johnwatson484@hotmail.com>
2021-03-09 13:01:15 +0000

    Debug configurations added

--- test/unit/app-insights.test.js
+++ test/unit/app-insights.test.js
@@ -1,6 +1,16 @@
-jest.mock('applicationinsights')
+jest.mock('applicationinsights', () => {
+  return {
+    setup: jest.fn().mockReturnThis(),
+    start: jest.fn(),
+    defaultClient: {
+      context: {
+        keys: [],
+        tags: []
+      }
+    }
+  }
+})
 const applicationInsights = require('applicationinsights')
-const appInsights = require('../../app/insights')
 
 describe('Application Insights', () => {
   const DEFAULT_ENV = process.env
@@ -16,7 +26,15 @@ describe('Application Insights', () => {
   })
 
   test('does not setup application insights if no instrumentation key', () => {
+    const appInsights = require('../../app/insights')
     appInsights.setup()
     expect(applicationInsights.setup.mock.calls.length).toBe(0)
+  })
+
+  test('does setup application insights if instrumentation key present', () => {
+    const appInsights = require('../../app/insights')
+    process.env.APPINSIGHTS_INSTRUMENTATIONKEY = 'test-key'
+    appInsights.setup()
+    expect(applicationInsights.setup.mock.calls.length).toBe(1)
   })
 })
