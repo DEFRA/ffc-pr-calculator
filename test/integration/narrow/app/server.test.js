d25f811b26f0708ef7ee8175dfe522b53044a1c6
John Watson <johnwatson484@hotmail.com>
2021-03-09 14:38:01 +0000

    Server tests added

--- test/integration/narrow/app/server.test.js
+++ test/integration/narrow/app/server.test.js
@@ -1,6 +1,30 @@
 describe('Server test', () => {
+  let server
+
+  beforeEach(() => {
+    jest.resetModules()
+    jest.mock('../../../../app/plugins/router', () => {
+      return {
+        plugin: {
+          name: 'mock-router',
+          register: () => {}
+        }
+      }
+    })
+  })
+
   test('createServer returns server', () => {
-    const server = require('../../../../app/server')
+    server = require('../../../../app/server')
+    expect(server).toBeDefined()
+  })
+
+  test('createServer returns server if dev', async () => {
+    jest.mock('../../../../app/config', () => {
+      return {
+        isDev: true
+      }
+    })
+    server = require('../../../../app/server')
     expect(server).toBeDefined()
   })
 })
