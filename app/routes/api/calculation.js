9672cacf9b174aeac3f7d867b728df6cd6ba254c
John Watson <johnwatson484@hotmail.com>
2021-03-13 17:07:15 +0000

    Remove BPS from api

--- app/routes/api/calculation.js
+++ app/routes/api/calculation.js
@@ -4,18 +4,18 @@ const boom = require('@hapi/boom')
 
 module.exports = [{
   method: 'GET',
-  path: '/api/v1/calculation/{bpsValue}',
+  path: '/api/v1/calculation/{value}',
   options: {
     validate: {
       params: joi.object({
-        bpsValue: joi.number().precision(2).greater(0).less(1000000000).required()
+        value: joi.number().precision(2).greater(0).less(1000000000).required()
       }),
       failAction: async (request, h, error) => {
         return boom.badRequest()
       }
     },
     handler: (request, h) => {
-      const result = calculateFromValue(request.params.bpsValue)
+      const result = calculateFromValue(request.params.value)
       return h.response(result)
     }
   }
