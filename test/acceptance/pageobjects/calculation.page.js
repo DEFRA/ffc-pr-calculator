f4df8b8845c05996702019092f833b6e81c26fd1
kaziyiola <kaz.iyiola@defra.gov.uk>
2021-03-24 10:14:40 +0000

    add standard js

--- test/acceptance/pageobjects/calculation.page.js
+++ test/acceptance/pageobjects/calculation.page.js
@@ -1,22 +1,20 @@
 import Page from './page'
 
 class CalculationPage extends Page {
-  
   // define elements
-  get header1 () { return $('h1.govuk-panel__title') }   
-  get cookiesLink () { return $('=Cookies'); }
-  get openGovtLink () { return $('=Open Government Licence v3.0'); }
-  get accessStatementLink () { return $('=Accessibility statement'); }
-  get copyRightLink () { return $('= © Crown copyright'); }      
+  get header1 () { return $('h1.govuk-panel__title') }
+  get cookiesLink () { return $('=Cookies') }
+  get openGovtLink () { return $('=Open Government Licence v3.0') }
+  get accessStatementLink () { return $('=Accessibility statement') }
+  get copyRightLink () { return $('= © Crown copyright') }
 
-  
   open () {
     super.open('')
     browser.pause(3000)
   }
 
   getHeaderText () {
-     this.header1.gettext()
+    this.header1.gettext()
   }
 }
 
