57b08a7b8d36b00c32cc3ac0aff7e4016adc15fc
John Watson <johnwatson484@hotmail.com>
2021-08-02 21:34:15 +0100

    Variable renamed

--- app/calculation/convert-currency.js
+++ app/calculation/convert-currency.js
@@ -1,12 +1,12 @@
-function convertStringToPence (valueInPence) {
-  const currencyArray = valueInPence.split('.')
+function convertStringToPence (valueInPounds) {
+  const currencyArray = valueInPounds.split('.')
   const pounds = currencyArray[0]
   const pence = (currencyArray[1] || '00').padEnd(2, '0')
   return Number(pounds + pence)
 }
 
-function convertToPounds (valueInPounds) {
-  return valueInPounds / 100
+function convertToPounds (valueInPence) {
+  return valueInPence / 100
 }
 
 module.exports = {
