514de3d9104d3820ccb4a99c763a1e31735314a6
John Watson <johnwatson484@hotmail.com>
2022-11-03 15:07:50 +0000

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -41,20 +41,16 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            outputPath: 'images/'
-          }
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
         }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            outputPath: 'fonts/'
-          }
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[fullhash].[ext]'
         }
       }
     ]
