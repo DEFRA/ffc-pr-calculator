616e10901c90c5172f67867c86677c33c9276e32
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-10-04 14:17:02 +0100

    removed commenting in model

--- app/routes/models/value.js
+++ app/routes/models/value.js
@@ -1,16 +1,6 @@
 function ViewModel (value, error) {
   // Constructor function to create logic dependent nunjucks page
   this.model = {
-    // label: {
-    //   text: 'Enter your BPS starting payment amount',
-    //   classes: 'govuk-label--m',
-    //   isPageHeading: true
-    // },
-    // hint: {
-    //   text: `This calculator will estimate your payment for the 2023 scheme year based on the starting payment amount you enter.
-    //   You will also see estimated payments for the 2021 and 2022 scheme years when you input your starting amount.
-    //   You should use the 'Sub total' in the 'Claim summary box' of your most recent BPS Claim Statement as the starting payment amount`
-    // },
     prefix: {
       text: '£'
     },
