69c430d03f751e0c2ea5bbcdcc7877c165b0f308
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-10-05 17:15:42 +0100

    change calculation model to mirror layout changes

--- app/plugins/router.js
+++ app/plugins/router.js
@@ -6,6 +6,7 @@ const routes = [].concat(
   require('../routes/value'),
   require('../routes/value-calculator'),
   require('../routes/calculation'),
+  require('../routes/calculation-bps'),
   require('../routes/cookies'),
   require('../routes/accessibility'),
   require('../routes/privacy'),
