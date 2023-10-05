9a91775b0136e811e65d0912c26db3b496698a4b
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-10-05 17:19:05 +0100

    change calculation model to mirror layout changes

---
+++ app/routes/calculation-bps.js
@@ -0,0 +1,21 @@
+const ViewModel = require('./models/calculation-bps')
+const calculate = require('../calculation')
+const schema = require('./schemas/value')
+
+module.exports = [{
+  method: 'GET',
+  path: '/calculation-bps',
+  options: {
+    validate: {
+      query: schema,
+      failAction: async (request, h, error) => {
+        return h.redirect('/value').takeover()
+      }
+    },
+    handler: (request, h) => {
+      const value = request.query.value || JSON.parse(JSON.stringify(request.query))
+      const result = calculate(value)
+      return h.view('calculation-bps', new ViewModel(value, result))
+    }
+  }
+}]
