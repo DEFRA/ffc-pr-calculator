fa7d836c10920d7dab4e74c5f6c7c5c2d49341ca
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-05-14 11:57:41 +0100

    updated app.test.js

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -1,13 +1,33 @@
-describe('Entry point test', () => {
-  test('entry point starts server', () => {
-    const mockStart = jest.fn()
-    jest.mock('../../app/server', () => jest.fn(() => {
-      return {
-        then: mockStart.mockReturnThis(),
-        catch: jest.fn()
-      }
-    }))
-    require('../../app')
-    expect(mockStart.mock.calls.length).toBe(1)
+const createServer = require('../../app/server')
+const config = require('../../app/config')
+
+describe('Server test', () => {
+  let server
+
+  beforeAll(async () => {
+    server = await createServer()
   })
+
+  test('server starts', () => {
+    expect(server).toBeDefined()
+  })
+
+  test('server is configured correctly', () => {
+    expect(server.info.port).toBe(config.port)
+  })
+
+  test('correct plugins are registered', async () => {
+    const plugins = server.registrations
+
+    expect(plugins).toHaveProperty('@hapi/vision')
+  })
+
+  if (config.isDev) {
+    test('correct plugins are registered in development mode', () => {
+      const plugins = server.registrations
+
+      expect(plugins).toHaveProperty('blipp')
+      expect(plugins).toHaveProperty('hapi-pino')
+    })
+  }
 })
