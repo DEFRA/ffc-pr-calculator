88b7fbe9d63829c0d1fb12ee1dece98fae20d1f6
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-11-28 14:15:40 +0000

    Doc 1656 change3 adding dates (#113)
    * updated text for years, created dynamic file and new tests
    
    * update test coverage
    
    * further test updates, added non tested config files to excludes
    
    * changed 2024 tab wording
    
    * package bump
    
    * tardy test update now fixed
    
    * adding dates to December and September closing payment

--- app/utils/year-tab-schedule.js
+++ app/utils/year-tab-schedule.js
@@ -1,8 +1,8 @@
 module.exports = [{
   year: 2025,
-  text: 'We plan to make the payment in 2 instalments - an advance payment of around 50% from 1 August with the balance payment from December.'
+  text: 'We plan to make the payment in 2 instalments - an advance payment of around 50% from 1 August with the balance payment from 1 December.'
 },
 {
   year: 2024,
-  text: 'We paid the 2024 delinked payment in 2 instalments from 1 August with the balance payment from September.'
+  text: 'We paid the 2024 delinked payment in 2 instalments from 1 August with the balance payment from 30 September.'
 }]
