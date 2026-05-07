276d74605e61c1b074bbc4a9a560f95cf0a7d177
Sam Plackett <samuel.plackett@eviden.com>
2026-05-07 17:07:28 +0100

    add full sitemap

---
+++ app/routes/models/sitemap.js
@@ -0,0 +1,12 @@
+function ViewModel (sections = []) {
+  this.model = {
+    title: { text: 'Sitemap' },
+    sections: (sections || []).map(s => {
+      const title = s.title || ''
+      const links = (s.links || []).map(l => (typeof l === 'string' ? { href: l, text: l } : l))
+      return { title, links }
+    })
+  }
+}
+
+module.exports = ViewModel
--- app/routes/sitemap.js
+++ app/routes/sitemap.js
@@ -1,25 +1,28 @@
-const { SitemapStream, streamToPromise } = require('sitemap')
-const config = require('../config')
+const ViewModel = require('./models/sitemap')
 
 module.exports = {
   method: 'GET',
-  path: '/sitemap.xml',
+  path: '/sitemap',
   options: {
-    auth: false,
-    handler: async (request, h) => {
-      const rawHost = process.env.SITE_HOSTNAME ?? `localhost:${config.port}`
-      const hostname = /^https?:\/\//i.test(rawHost) ? rawHost : `http://${rawHost}`
-      const smStream = new SitemapStream({ hostname })
+    handler: (request, h) => {
+      const sections = [
+        {
+          title: '',
+          links: [
+            { href: '/', text: 'Home' }
+          ]
+        },
+        {
+          title: 'Help',
+          links: [
+            { href: '/accessibility', text: 'Accessibility statement' },
+            { href: '/cookies', text: 'Cookies' },
+            { href: '/privacy', text: 'Privacy' }
+          ]
+        }
+      ]
 
-      const routes = request.server.table()
-        .filter(r => r.method.toUpperCase() === 'GET')
-        .map(r => r.path)
-        .filter(p => !p.includes('{') && !p.startsWith('/static') && p !== '/sitemap.xml')
-
-      routes.forEach(path => smStream.write({ url: path }))
-      smStream.end()
-      const xml = (await streamToPromise(smStream)).toString()
-      return h.response(xml).type('application/xml')
+      return h.view('sitemap', new ViewModel(sections))
     }
   }
 }
